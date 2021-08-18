8e6ddba99e416e8fa11a9e02a201b9b6efdfe399
NeagMircea <aluructosh@gmail.com>
2021-08-18 10:41:26 +0300

    Added project scaffold
    Added WPF(&CBMicro) and SSDT scaffold to the project

---
+++ WpfDesktopUI/Bootstrapper.cs
@@ -0,0 +1,29 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WpfDesktopUI.ViewModels;
+
+namespace WpfDesktopUI
+{
+    /// <summary>
+    /// Class used to configure Caliburn Micro
+    /// </summary>
+    public class Bootstrapper : BootstrapperBase
+    {
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}

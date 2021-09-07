5f8cbec39fbf2e21da02d68eb226c91638d6e4bb
NeagMircea <aluructosh@gmail.com>
2021-09-08 00:04:50 +0300

    Created Subcategory View
    Added view for subcategory management

---
+++ WpfDesktopUI/EventModels/GoSubcategoryViewEvent.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDesktopUI.EventModels
+{
+    public class GoSubcategoryViewEvent
+    {
+        public int ProgramId { get; set; }
+        public string ProgramName { get; set; }
+
+        public int WorkoutId { get; set; }
+        public string WorkoutName { get; set; }
+
+        public int DayId { get; set; }
+        public string DayName { get; set; }
+
+        public int CategoryId { get; set; }
+    }
+}
---
+++ WpfDesktopUI/ViewModels/SubcategoryViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfDesktopUI.Views.Interfaces;
+using WpfDesktopUI.Views.Interfaces.Composite;
+
+namespace WpfDesktopUI.ViewModels
+{
+    class SubcategoryViewModel : Screen/*, IAddView, IAddSelectedItem*/
+    {
+
+
+        //public bool CanAddNew => throw new NotImplementedException();
+
+        //public bool CanRemoveSelected => throw new NotImplementedException();
+
+        //public bool IsErrorVisible => throw new NotImplementedException();
+
+        //public string ErrorMessage { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        //public bool CanAddSelected => throw new NotImplementedException();
+
+        //public void AddNew()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void AddSelected()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public Task GoBack()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void LoadItems()
+        //{
+        //    throw new NotImplementedException();
+        //}
+
+        //public void RemoveSelected()
+        //{
+        //    throw new NotImplementedException();
+        //}
+    }
+}
